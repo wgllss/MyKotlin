34e287a6e70f50d8881f3ef2c189c1664ddc9418
wgllss <283495283@qq.com>
2023-05-27 15:42:55 +0800

    flow zip 动态多个

--- app/src/main/java/com/example/myapplication/ui/main/activity/TestNetActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/main/activity/TestNetActivity.kt
@@ -9,6 +9,8 @@ import com.example.myapplication.ex.logE
 import com.example.myapplication.net.impl.RequestBodyWrapper
 import com.scclzkj.api.Api
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody
@@ -32,11 +34,57 @@ class TestNetActivity : BaseActivity(), View.OnClickListener {
 
     }
 
+    fun onClick3(v: View?) {
+//        val map = mutableMapOf<String, Flow<Any>>()
+        var flowZip: Flow<Any>? = null
+        var flow: Flow<Int>? = null
+        val flow1 = flow { emit(1) }
+        val flow2 = flow { emit(2) }
+        val flow3 = flow { emit(3) }
+        val flow4 = flow { emit(4) }
+        val flow5 = flow { emit(5) }
+        val flow6 = flow { emit(6) }
+        val flow7 = flow { emit(7) }
+        val flow8 = flow { emit(8) }
+
+        val flows = mutableMapOf<String, Flow<Int>>(
+            "flowkey1" to flow1,
+            "flowkey2" to flow2,
+            "flowkey3" to flow3,
+            "flowkey4" to flow4,
+            "flowkey5" to flow5,
+            "flowkey6" to flow6,
+            "flowkey7" to flow7,
+            "flowkey8" to flow8
+        )
+
+        flows.forEach {
+            if (flow == null) {
+                flow = it.value
+            } else {
+                if (flowZip == null) {
+                    flowZip = flow!!.zip(it.value) { it1, it2 ->
+                        logE("打印 it1: $it1   it2:$it2")
+                    }
+                } else {
+                    flowZip = flowZip!!.zip(it.value) { _, it2 ->
+                        logE("打印    it2:$it2")
+                    }
+                }
+            }
+        }
+        lifecycleScope.launch {
+            flowZip!!.catch { it.printStackTrace() }
+                .flowOn(Dispatchers.IO)
+                .collect()
+        }
+    }
+
     fun onClick1(v: View?) {
         lifecycleScope.launch {
             val result = api.getMusic()
 
-            logE("result---> ${   result.raw().request.url}")
+            logE("result---> ${result.raw().request.url}")
         }
     }
 
