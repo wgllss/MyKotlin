56c4dd3bce78aae385bd34abd70f742379f8cb2a
wgllss <283495283@qq.com>
2023-06-02 17:03:07 +0800

    exoplayer 2.8.0

--- app/src/main/java/com/example/myapplication/ui/main/activity/ExoplayerActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/main/activity/ExoplayerActivity.kt
@@ -1,13 +1,11 @@
 package com.example.myapplication.ui.main.activity
 
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
-import com.google.android.exoplayer2.DefaultLoadControl
-import com.google.android.exoplayer2.DefaultRenderersFactory
-import com.google.android.exoplayer2.ExoPlayerFactory
-import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.source.ExtractorMediaSource
 import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
@@ -17,8 +15,9 @@ import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 
 class ExoplayerActivity : AppCompatActivity() {
 
-    private lateinit var player: SimpleExoPlayer
+    private lateinit var player: ExoPlayer
 
+//    private lateinit var playerView: StyledPlayerView
     private lateinit var video_view: PlayerView
 
     //    private val url = "https://pgcvideo-cdn.xiaodutv.com/1910403440_1018264544_20190318012008.mp4?Cache-Control%3Dmax-age%3A8640000%26responseExpires%3DWed%2C_26_Jun_2019_01%3A20%3A09_GMT=&xcode=6e1b252a3895d89330027ee04ef815ae9fde6ba2a22f6493&time=1685758789&sign=1685672389-8419912-0-1fd289ddbb7d7df475cfc6b45517d58f&_=1685674557140"
@@ -27,6 +26,9 @@ class ExoplayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exoplayer)
+
+//        playerView = findViewById(R.id.player_view)
+//        playerView.requestFocus()
 
         player = ExoPlayerFactory.newSimpleInstance(
             DefaultRenderersFactory(this),
@@ -47,5 +49,31 @@ class ExoplayerActivity : AppCompatActivity() {
     private fun buildMediaSource(uri: Uri): MediaSource {
         val dataSourceFactory = DefaultDataSourceFactory(this, "com.example.exoplayerdemo")
         return ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(uri)
+    }
+
+    override fun onStart() {
+        super.onStart()
+//        if (Build.VERSION.SDK_INT > 23) {
+//            initializePlayer()
+//            if (playerView != null) {
+//                playerView.onResume()
+//            }
+//        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+//        if(video_view!=null){
+//            video_view.
+//        }
+    }
+
+    override fun onStop() {
+        super.onStop()
     }
 }
